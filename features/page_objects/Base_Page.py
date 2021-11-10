28549fd405ddc945b303ca66ad408d1948245fb3
Animesh_Mukherjee <er.animesh6@hotmail.com>
2021-11-10 13:08:34 +0530

    Scenario converted to Page Object Model.

--- features/page_objects/Base_Page.py
+++ features/page_objects/Base_Page.py
@@ -1,8 +1,8 @@
 from selenium.webdriver import ActionChains
 from selenium.webdriver.support.select import Select
-from BehaveBDDWithPageObjects.Utilities import configReader
+from Utilities.config_reader import config_reader
 import logging
-from BehaveBDDWithPageObjects.Utilities.LogUtil import Logger
+from Utilities.run_log import Logger
 
 log = Logger(__name__, logging.INFO)
 
@@ -14,30 +14,30 @@ class BasePage:
 
     def click(self, locator):
         if str(locator).endswith("_XPATH"):
-            self.driver.find_element_by_xpath(configReader.readConfig("locators", locator)).click()
+            self.driver.find_element_by_xpath(config_reader("locators", locator)).click()
         elif str(locator).endswith("_CSS"):
-            self.driver.find_element_by_css_selector(configReader.readConfig("locators", locator)).click()
+            self.driver.find_element_by_css_selector(config_reader("locators", locator)).click()
         elif str(locator).endswith("_ID"):
-            self.driver.find_element_by_id(configReader.readConfig("locators", locator)).click()
+            self.driver.find_element_by_id(config_reader("locators", locator)).click()
         log.logger.info("Clicking on an element: " + str(locator))
 
     def type(self, locator, value):
         if str(locator).endswith("_XPATH"):
-            self.driver.find_element_by_xpath(configReader.readConfig("locators", locator)).send_keys(value)
+            self.driver.find_element_by_xpath(config_reader("locators", locator)).send_keys(value)
         elif str(locator).endswith("_CSS"):
-            self.driver.find_element_by_css_selector(configReader.readConfig("locators", locator)).send_keys(value)
+            self.driver.find_element_by_css_selector(config_reader("locators", locator)).send_keys(value)
         elif str(locator).endswith("_ID"):
-            self.driver.find_element_by_id(configReader.readConfig("locators", locator)).send_keys(value)
+            self.driver.find_element_by_id(config_reader("locators", locator)).send_keys(value)
         log.logger.info("Typing in an element: " + str(locator) + " value entered as : " + str(value))
 
     def select(self, locator, value):
         global dropdown
         if str(locator).endswith("_XPATH"):
-            dropdown = self.driver.find_element_by_xpath(configReader.readConfig("locators", locator))
+            dropdown = self.driver.find_element_by_xpath(config_reader("locators", locator))
         elif str(locator).endswith("_CSS"):
-            dropdown = self.driver.find_element_by_css_selector(configReader.readConfig("locators", locator))
+            dropdown = self.driver.find_element_by_css_selector(config_reader("locators", locator))
         elif str(locator).endswith("_ID"):
-            dropdown = self.driver.find_element_by_id(configReader.readConfig("locators", locator))
+            dropdown = self.driver.find_element_by_id(config_reader("locators", locator))
         select = Select(dropdown)
         select.select_by_visible_text(value)
         log.logger.info("Selecting from an element: " + str(locator) + " value selected as : " + str(value))
@@ -45,11 +45,11 @@ class BasePage:
     def move_to(self, locator):
 
         if str(locator).endswith("_XPATH"):
-            element = self.driver.find_element_by_xpath(configReader.readConfig("locators", locator))
+            element = self.driver.find_element_by_xpath(config_reader("locators", locator))
         elif str(locator).endswith("_CSS"):
-            element = self.driver.find_element_by_css_selector(configReader.readConfig("locators", locator))
+            element = self.driver.find_element_by_css_selector(config_reader("locators", locator))
         elif str(locator).endswith("_ID"):
-            element = self.driver.find_element_by_id(configReader.readConfig("locators", locator))
+            element = self.driver.find_element_by_id(config_reader("locators", locator))
         action = ActionChains(self.driver)
         action.move_to_element(element).perform()
         log.logger.info("Moving to an element: " + str(locator))
