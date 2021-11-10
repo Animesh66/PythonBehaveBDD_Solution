6331ee55c846ee83f13546ae3b46c850b3deb7eb
Animesh_Mukherjee <er.animesh6@hotmail.com>
2021-11-10 10:43:56 +0530

    Scenario converted to Page Object Model.

--- Utilities/config_reader.py
+++ Utilities/config_reader.py
@@ -1,3 +1,7 @@
 from configparser import ConfigParser
 
+
 def config_reader(section, key):
+    config = ConfigParser()
+    config.read(".\\Config_Data\\config.ini")
+    return config.get(section, key)
--- features/environment.py
+++ features/environment.py
@@ -1,4 +1,4 @@
 from selenium import webdriver
-
+from Utilities.config_reader import config_reader
 
 def before_scenario(context, driver):
